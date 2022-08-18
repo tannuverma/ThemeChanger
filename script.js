d47f03297c5044679283f66a987aa1521dff4342
tannuverma <73612363+tannuverma@users.noreply.github.com>
2022-08-18 14:50:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $("#blue").click(function () {
+      $(".data").css("backgroundColor", "blue");
+      $("#textData").css("color", "white");
+      $("#textData").css("backgroundColor", "blue");
+      $("#textData").css("borderColor", "white");
+    });
+    $("#black").click(function () {
+      $(".data").css("backgroundColor", "black");
+      $("#textData").css("color", "white");
+      $("#textData").css("backgroundColor", "black");
+      $("#textData").css("borderColor", "white");
+    });
+    $("#yellow").click(function () {
+      $(".data").css("backgroundColor", "yellow");
+      $("#textData").css("color", "black");
+      $("#textData").css("backgroundColor", "yellow");
+      $("#textData").css("borderColor", "black");
+    });
+    $("#green").click(function () {
+      $(".data").css("backgroundColor", "green");
+      $("#textData").css("color", "black");
+      $("#textData").css("backgroundColor", "green");
+      $("#textData").css("borderColor", "black");
+    });
+  });
